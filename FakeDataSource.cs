29c798895aa4983b88551bd473a80bc4e55e5dec
Dave Hanna <dave@infotouchsys.com>
2013-03-16 21:25:35 -0500

    Break out test support files from PhotoServer_Tests project

---
+++ FakeDataSource.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhotoServer.DataAccessLayer;
+using PhotoServer.Domain;
+
+namespace RacePhotosTestSupport
+{
+    public class FakeDataSource : IPhotoDataSource
+    {
+        private FakeRepository<Photo>  _photoData;
+        public IRepository<Photo, Guid> photoData { get { return _photoData; } }
+
+        public FakeDataSource()
+        {
+            _photoData = new FakeRepository<Photo>();
+        }
+
+	    public int SaveChanges()
+	    {
+		    return _photoData.SaveChanges();
+	    }
+    }
+
+   
+}
