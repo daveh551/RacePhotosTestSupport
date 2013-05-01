eac0bbe7d40efa4759065558ffe14b6bc17d707d
Dave Hanna <dave@infotouchsys.com>
2013-04-30 20:24:49 -0500

    Move FakeControllerContext, FakeHttpContext into TestSupport

---
+++ FakeControllerContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Mvc;
+
+namespace RacePhotosTestSupport
+ 
+{
+    public class FakeControllerContext : HttpControllerContext
+    {
+	    public FakeControllerContext()
+	    {
+		    Configuration = new HttpConfiguration();
+			
+			
+	    }
+
+        
+ 
+    }
+
+
+
+}
