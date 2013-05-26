5ec018cc61ef9dd3182690baac69e6a9a86c2810
Dave Hanna <dave@infotouchsys.com>
2013-05-25 20:39:23 -0500

    Revisions to test data
    Revise TestData path in ObjectMother
    Revise FakeDataSource to match new Repositories
    Add mocking for HttpContext.User property
    
     On branch master
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   FakeDataSource.cs
    	modified:   FakeHttpContext.cs
    	new file:   FakeIntReferenceRepository.cs
    	modified:   ObjectMother.cs
    	modified:   RacePhotosTestSupport.csproj
    	new file:   packages.config

--- FakeHttpContext.cs
+++ FakeHttpContext.cs
@@ -2,17 +2,36 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using Moq;
 
 namespace RacePhotosTestSupport
 {
 	public class FakeHttpContext : HttpContextBase
 	{
+		public FakeHttpContext()
+		{
+
+			var mockIdentity = new Mock<IIdentity>();
+				mockIdentity.SetupGet(id => id.Name)
+				.Returns("FinishLineAdmin");
+				IIdentity identity = mockIdentity.Object;
+			_user = new ClaimsPrincipal(identity);
+		}
 		public override HttpServerUtilityBase Server
 		{
 			get { return new FakeHttpServerUtility(); }
+		}
+
+		private IPrincipal _user;
+		public override System.Security.Principal.IPrincipal User
+		{
+			get { return _user; }
 		}
 	}
 
